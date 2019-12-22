662d8d6f7326c29922dc2671b4098e462101b54d
sagar1976 <narvekar.sagar@gmail.com>
2019-12-22 17:53:11 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ class App extends Component {
   render() {
     return (
       <div>
+      <div className="header">
         Phone Directory
       </div>
-    )
+      <button>Add</button>
+      <div>
+        <span>Name</span><br />
+        <span>Phone</span>
+      </div>
+      </div>
+    );
   }
 }
 
