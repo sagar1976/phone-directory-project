fb56b29a22959cf3087f079af98e6eab538e3d84
sagar1976 <narvekar.sagar@gmail.com>
2019-12-23 21:36:30 +0530

    Header’ Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, {Component} from 'react';
 import { render } from '@testing-library/react';
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
       <div>
-      <div className="header">
-        Phone Directory
-      </div>
-      <button>Add</button>
-      <div>
-        <span>Name</span><br />
-        <span>Phone</span>
-      </div>
+          <Header />
+          <button>Add</button>
+          <span>Name</span><br />
       </div>
     );
   }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = function() {
+    return(
+        <div className="header">
+            PhoneDirectory
+        </div>
+    )
+}
+
+export default Header;
