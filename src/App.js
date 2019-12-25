8b9dfd3455befa804339652a9fe7546929a192cc
sagar1976 <narvekar.sagar@gmail.com>
2019-12-25 10:29:29 +0530

    styling for body of phone directory

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
 import { render } from '@testing-library/react';
 import Header from './Header';
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div>
           <Header />
-          <button>Add</button>
-          <span>Name</span><br />
+          <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+      </div>
+      </div>
       </div>
     );
   }
